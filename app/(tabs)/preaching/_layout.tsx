c9c81c7f4f2cbf9e707a502f4496f1bb92862259
Paul Swagerty <pswagerty@gmail.com>
2025-10-27 13:00:04 -0700

    Improving the podcast playback

--- app/(tabs)/preaching/_layout.tsx
+++ app/(tabs)/preaching/_layout.tsx
@@ -101,7 +101,14 @@ export default function PreachingLayout() {
         options={{
           headerTitle: 'Podcast',
           headerBackTitle: '',
-          headerLeft: () => headerLeftComponent,
+          headerRight: () => headerRightComponent,
+        }}
+      />
+      <Stack.Screen 
+        name="episode/[id]" 
+        options={{
+          headerTitle: 'Episode',
+          headerBackTitle: '',
           headerRight: () => headerRightComponent,
         }}
       />
